e83af20cc80db3b4f26e4d5533690f171147ecc7
Joey Dong <joeydong0828@gmail.com>
2018-01-22 02:38:50 +0800

    first_try

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Feature Photo Slicer
+<iframe src="https://drive.google.com/file/d/1C2yw6jgK009dkOLK9MRHPu4RfGtNNvu-/preview" width="640" height="480">
+</iframe>
+
+## Environment
+**OS**: Linux / Unix / Windows (Not sure)
+**Python**: 3.6 (Anaconda) , OpenCV3
+
+## Steps:
+1. Put the code and the photo which you want to slice in the same folder
+2. Modify the start X-axis to slice in the code 'split_fb.py' (Initail is 0)
+3. Run in terminal
+    ``` 
+    python split_fb.py
+    ```
+4. You will get sliced photo in ./slit folder
+5. Upload 5 image to your Facebook Feature Photo. 
+The Sequence is split_img1 to 5
---
+++ split_fb.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np 
+
+img = cv2.imread('35592238096_b4cd272659_o.jpg')
+height = np.shape(img)[0]
+x_y = np.shape(img)[1] - np.shape(img)[0]
+#x_start don't > x_y
+x_start = 0
+
+split_img1 = img[:int(height/2), x_start:int(x_start+(height/2)), :]
+split_img2 = img[int(height/2):, x_start:int(x_start+(height/2)), :]
+split_img3 = img[:int(height/3), int(x_start+(height/2)):-(x_y - x_start), :]
+split_img4 = img[int(height/3):int(2 * height/3), int(x_start+(height/2)):-(x_y - x_start), :]
+split_img5 = img[int(2 * height/3): , int(x_start+(height/2)):-(x_y - x_start), :]
+
+cv2.imwrite('./split/split_img1.png',split_img1)
+cv2.imwrite('./split/split_img2.png',split_img2)
+cv2.imwrite('./split/split_img3.png',split_img3)
+cv2.imwrite('./split/split_img4.png',split_img4)
+cv2.imwrite('./split/split_img5.png',split_img5)
